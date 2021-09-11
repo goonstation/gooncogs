2f97f207930d1f6259998500a4fca0a575d2c4ca
Sovexe <33204415+Sovexe@users.noreply.github.com>
2021-09-11 19:53:23 -0400

    no arg either

--- editableposts/editableposts.py
+++ editableposts/editableposts.py
@@ -58,7 +58,7 @@ class EditablePosts(commands.Cog):
     async def remove(self, ctx: commands.Context, message: discord.Message):
         if not await self.valid_message(message):
             return
-        await message.delete(message)
+        await message.delete()
         await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
 
     @editable_posts.command()
