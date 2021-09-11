0d9c29546d0207b374483be35c8c3bc3cd0ab9a7
pali6 <6pali6@gmail.com>
2021-09-12 00:25:05 +0200

    better text normalization for searching

--- roundreminder/roundreminder.py
+++ roundreminder/roundreminder.py
@@ -14,11 +14,14 @@ class RoundReminder(commands.Cog):
         self.config = Config.get_conf(self, identifier=77984122151871643842)
         self.config.register_user(**self.default_user_settings)
 
+    def normalize(self, text):
+        return ''.join(c for c in text.lower() if c.isalnum())
+
     @commands.command()
     async def nextround(self, ctx: commands.Context, search_text: Optional[str]):
         """Notifies you about the next round or the next round with server or map name containing `search_text`."""
         async with self.config.user(ctx.author).match_strings() as match_strings:
-            match_strings.append(search_text)
+            match_strings.append(self.normalize(search_text))
         await ctx.message.add_reaction("\N{WHITE HEAVY CHECK MARK}")
 
     async def notify(self, user: discord.User, message: discord.Message, match_string: str):
@@ -32,7 +35,7 @@ class RoundReminder(commands.Cog):
             
             embed = message.embeds[0]
             fulltext = ' '.join([embed.title] + [embed.description] + [f.name + " " + f.value for f in embed.fields])
-            fulltext = fulltext.lower()
+            fulltext = self.normalize(fulltext)
 
             for user_id, data in (await self.config.all_users()).items():
                 match_strings = data['match_strings']
