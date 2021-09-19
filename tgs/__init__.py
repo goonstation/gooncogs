89d184b8ac341568ecbbd0f2c075b1505eb9663d
pali6 <6pali6@gmail.com>
2021-09-19 11:56:53 +0200

    typo in example url

--- tgs/__init__.py
+++ tgs/__init__.py
@@ -4,7 +4,7 @@ from .tgs import TGS
 async def setup(bot: Red):
     api_tokens = await bot.get_shared_api_tokens('test')
     if 'user' not in api_tokens or 'password' not in api_tokens or 'host' not in api_tokens:
-        await ctx.send("**Cog not loaded!**\nYou need to set RedBot API keys for your TGS instance like this:\n`]set api tgs user,exampleusername password,examplepassword host,https://tgs.exampls.com`")
+        await ctx.send("**Cog not loaded!**\nYou need to set RedBot API keys for your TGS instance like this:\n`]set api tgs user,exampleusername password,examplepassword host,https://tgs.example.com`")
         return
     cog = TGS(bot)
     bot.add_cog(cog)
