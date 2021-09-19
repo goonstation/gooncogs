1e06d1132d22f6ec459eee976663e7b766b80de7
pali6 <6pali6@gmail.com>
2021-09-19 12:05:07 +0200

    I totally didn't forget to change the API token id from 'test'

--- tgs/__init__.py
+++ tgs/__init__.py
@@ -3,9 +3,12 @@ from redbot.core.errors import CogLoadError
 from .tgs import TGS
 
 async def setup(bot: Red):
-    api_tokens = await bot.get_shared_api_tokens('test')
-    if 'user' not in api_tokens or 'password' not in api_tokens or 'host' not in api_tokens:
-        raise CogLoadError( "You need to set RedBot API keys for your TGS instance like this:\n`]set api tgs user,exampleusername password,examplepassword host,https://tgs.example.com`")
+    api_tokens = await bot.get_shared_api_tokens('tgs')
+    required_keys = {'user', 'password', 'host'}
+    if any(key not in api_tokens for key in required_keys):
+        message = "Missing TGS API keys: " + ', '.join(required_keys - api_tokens.keys()) + "\n"
+        message += "You need to set RedBot API keys for your TGS instance like this:\n`]set api tgs user,exampleusername password,examplepassword host,https://tgs.example.com`"
+        raise CogLoadError(message)
     cog = TGS(bot)
     bot.add_cog(cog)
 
