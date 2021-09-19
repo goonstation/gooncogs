710af143c23c6d0cb8fee10fd91af0fc584c5ba8
pali6 <6pali6@gmail.com>
2021-09-19 12:01:06 +0200

    apparently that's not how you do cog load errors

--- tgs/__init__.py
+++ tgs/__init__.py
@@ -1,11 +1,11 @@
 from redbot.core.bot import Red
+from redbot.core.errors import CogLoadError
 from .tgs import TGS
 
 async def setup(bot: Red):
     api_tokens = await bot.get_shared_api_tokens('test')
     if 'user' not in api_tokens or 'password' not in api_tokens or 'host' not in api_tokens:
-        await ctx.send("**Cog not loaded!**\nYou need to set RedBot API keys for your TGS instance like this:\n`]set api tgs user,exampleusername password,examplepassword host,https://tgs.example.com`")
-        return
+        raise CogLoadError( "You need to set RedBot API keys for your TGS instance like this:\n`]set api tgs user,exampleusername password,examplepassword host,https://tgs.example.com`")
     cog = TGS(bot)
     bot.add_cog(cog)
 
