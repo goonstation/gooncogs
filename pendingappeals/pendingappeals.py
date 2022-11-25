c4dbb53e18b88c73addbd12b0a936c89e221f1d6
pali6 <6pali6@gmail.com>
2022-11-25 23:01:40 +0000

    pendingappeals command now supports Waiting For Player Response label

--- pendingappeals/pendingappeals.py
+++ pendingappeals/pendingappeals.py
@@ -48,6 +48,7 @@ class PendingAppeals(commands.Cog):
             return res.status == 200
 
     async def test_thread(self, elem, labels_only=True):
+        waiting_for_player_response = False
         try:
             if "forumdisplay_sticky" in elem.find_parent("td").get_attribute_list(
                 "class"
@@ -56,23 +57,36 @@ class PendingAppeals(commands.Cog):
             if "label" in elem.previousSibling.previousSibling.get_attribute_list(
                 "class"
             ):
-                return None
+                label_elem = elem.previousSibling.previousSibling
+                if label_elem.text.strip() == "Waiting For Player Response":
+                    waiting_for_player_response = True
+                else:
+                    return None
         except:
             pass
         tokens = await self.bot.get_shared_api_tokens("mybb")
         if elem.a is None:
             return None
         url = tokens.get('url') + elem.a.get_attribute_list("href")[0]
-        if not labels_only:
+        if not labels_only or waiting_for_player_response:
             async with self.session.get(url) as res:
                 bs = BeautifulSoup(await res.text(), "html")
+                admin_replied = False
+                last_is_player = True
                 for auth_info in bs.find_all(class_="author_information"):
                     try:
                         rank = auth_info.find(class_="smalltext").text.strip().lower()
                         if "admin" in rank or "developer" in rank:
-                            return None
+                            admin_replied = True
+                            last_is_player = False
+                        else:
+                            last_is_player = True
                     except:
                         pass
+                if waiting_for_player_response and not last_is_player:
+                    return None
+                elif not waiting_for_player_response and not labels_only and admin_replied:
+                    return None
         return f"<{url}> {elem.a.text}"
 
     async def scrape_page(self, page, forum_id, labels_only=True):
