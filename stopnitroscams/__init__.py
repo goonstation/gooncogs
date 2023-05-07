75e82eb8fbaf37c48d6296414d727905e38f0e6b
rex2630 <rex2630@gmail.com>
2023-05-07 15:04:14 +0200

    Update to support Redbot 3.5.1

--- stopnitroscams/__init__.py
+++ stopnitroscams/__init__.py
@@ -2,5 +2,5 @@ from redbot.core.bot import Red
 from .stopnitroscams import StopNitroScams
 
 
-def setup(bot: Red):
-    bot.add_cog(StopNitroScams(bot))
+async def setup(bot: Red):
+    await bot.add_cog(StopNitroScams(bot))
