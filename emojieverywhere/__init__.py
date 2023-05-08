1386b91ab2150bd2614fbfcfa3017022a29f9ab7
pali6 <6pali6@gmail.com>
2023-05-08 12:37:22 +0000

    emojieverywhere now works

--- emojieverywhere/__init__.py
+++ emojieverywhere/__init__.py
@@ -1,11 +1,6 @@
 from redbot.core.bot import Red
 from .emojieverywhere import EmojiEverywhere
-from discord_slash import SlashCommand
 
 
 async def setup(bot: Red):
-    if not hasattr(bot, "slash"):
-        SlashCommand(
-            bot, sync_commands=True, override_type=True, sync_on_cog_reload=True
-        )
     await bot.add_cog(EmojiEverywhere(bot))
