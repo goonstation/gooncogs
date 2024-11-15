2f94477331fc7cdac99b616db9624929bbb697bc
Tarmunora <48066662+Tarmunora@users.noreply.github.com>
2024-11-15 13:48:46 -0600

    notes2

--- goonhub/goonhub.py
+++ goonhub/goonhub.py
@@ -13,6 +13,7 @@ import inspect
 import urllib.parse
 import uuid
 import time
+import json
 
 class GoonHub(commands.Cog):
     def __init__(self, bot: Red):
@@ -259,4 +260,134 @@ class GoonHub(commands.Cog):
             await menu(ctx, pages, DEFAULT_CONTROLS, timeout=60.0)
         else:
             await ctx.send(embed=pages[0])
+
+
+    @commands.command()
+    @checks.admin()
+    async def notes2(self, ctx: commands.Context, ckey):
+        ckey = self.ckeyify(ckey)
+        v = NotesBuilderView(self.bot, ckey)
+        async with ctx.typing():
+            try:
+                embed = await v.fetch_first_page()
+                await ctx.send(embed=embed, view = v)
+            except APIError as e:
+                await ctx.send(f"Error code {e} occured when querying the API")
+
+class APIError(Exception):
+    pass
+
+class NotesBuilderView(discord.ui.View):
+    def __init__(self, bot, ckey):
+        super().__init__(timeout = 30)
+        self.bot = bot
+        self.ckey = ckey
+        self.final_page = 1
+        self.fields = list()
+        self.embeds = list()
+        self.cont = ""
+        self.current_page = 0
+        self.fin = False
+        self.embed_idx = 0
+
+    @discord.ui.button(label="previous")
+    async def prev_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.embed_idx > 0:
+            self.embed_idx -= 1
+        self.update_buttons()
+        await interaction.response.edit_message(embed=self.embeds[self.embed_idx], view=self)
+
+    @discord.ui.button(label="next")
+    async def next_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if self.embed_idx < len(self.embeds) - 1:
+            await interaction.response.defer()
+            self.embed_idx += 1
+        elif not self.fin:
+            print("building")
+            try:
+                await interaction.response.defer(thinking=True)
+                self.embeds.append(await self.build_page())
+                followup = await interaction.followup.send("goodbye", wait=True)
+                await followup.delete()
+            except APIError as e:
+                await interaction.followup.send(f"Error code {e} occured when querying the API")
+                self.stop()
+                await interaction.message.edit(view=None)
+                return
+            self.embed_idx += 1
+        self.update_buttons()
+        await interaction.message.edit(embed=self.embeds[self.embed_idx], view=self)
+
+    @discord.ui.button(label="dismiss", style=discord.ButtonStyle.red)
+    async def dismiss_button(self, interaction: discord.Interaction, button: discord.ui.Button):
+        self.stop()
+        await interaction.message.delete()
+
+    def update_buttons(self):
+        self.children[0].disabled = self.embed_idx == 0
+        self.children[1].disabled = self.fin and self.embed_idx == len(self.embeds) - 1
+
+    async def fetch_first_page(self) -> discord.Embed:
+        embed = await self.build_page()
+        self.embeds.append(embed)
+        self.update_buttons()
+        return embed
+
+    async def build_page(self) -> discord.Embed:
+        title = f'notes for {self.ckey}'
+        curr_size = len(title)
+
+        embed = discord.Embed(title=title, color=discord.Color.from_str("#ff2222"))
+        pagenames = list()
+
+        def add_field(field_data):
+            nonlocal embed, curr_size
+            curr_size += field_data[0]
+            embed.add_field(name = field_data[1], value = field_data[2], inline = False)
+
+        while len(embed.fields) < 10: #loop until 10 fields - can also break if charlimit is reached
+            if len(self.fields) == 0: #if we are out of fields to append, fetch another batch
+                if self.current_page == self.final_page: #if we are out of pages to fetch, also break
+                    break
+                else:
+                    self.extract_notes(await self.fetch_notes_page(self.current_page + 1))
+
+            if curr_size + self.fields[0][0] > 5900: #if next field would put us over, finish adding fields
+                break
+            add_field(self.fields.pop(0))
+            pagenames.append(f"{self.current_page}{self.cont}")
+
+        embed.set_footer(text = f"page {pagenames[0]}{f' - {pagenames[-1]}'  if pagenames[0]!=pagenames[-1] else ''} of {self.final_page}")
+        self.cont = " (cont)" #for next embed, mark as continuing previous page
+        if len(self.fields) == 0 and self.current_page == self.final_page:
+            self.fin = 1
+        return embed
+
+    def extract_notes(self, data):
+        self.final_page = data["meta"]["last_page"] #fetching a new page, update our metas and mark as starting a new page
+        self.current_page = data["meta"]["current_page"]
+        self.cont = ""
+        for note in data["data"]:
+            time = note["updated_at"]
+
+            try:
+                time = f"<t:{int(datetime.datetime.strptime(time, '%Y-%m-%dT%H:%M:%SZ').timestamp())}:F>"
+            except:
+                pass
+
+            name = f'[{note["server_id"]}]: {note["game_admin"]["ckey"]} at {time}' #default name
+            for i, field_value in enumerate(pagify(note["note"], delims=('\n', ' '), priority=True, page_length=1024)):
+                field_name = name
+                if i == 1:
+                    field_name = "..."
+                elif i > 1:
+                    field_name = f"... ({i})"
+                field_size = len(field_name) + len(field_value)
+                self.fields.append((field_size, field_name, field_value))
+        return 1
+
+    async def fetch_notes_page(self, page) -> dict:
+        with open(f'Documents/goonstation/gooncogs/goonhub/mocks/{self.ckey}/{page-1}.json') as f:
+            d = json.load(f)
+        return d
 
